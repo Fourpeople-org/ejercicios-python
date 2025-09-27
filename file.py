2ef1e21856baac2214a5e26606091fa585c35f25
dvdm12 <dvddavid677@gmail.com>
2025-09-27 16:58:22 -0500

    i've added a loop

--- file.py
+++ file.py
@@ -4,3 +4,7 @@ def repeat_word(word: str, n: int) -> str:
 def is_even(number: int) -> bool:
     """Verifica si un número es par."""
     return number % 2 == 0
+
+def loop():
+   for i in range(1,10):
+     print(i)
