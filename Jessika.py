9a6df5fc94cdb6ddc161d6e5eb85de95d2d1726b
JessikaFernanda2908 <160680853+JessikaFernanda2908@users.noreply.github.com>
2025-09-27 16:35:42 -0500

    Mensaje descriptivo

---
+++ Jessika.py
@@ -0,0 +1,9 @@
+def verificar_par_impar():
+    numero = int(input("Ingresa un número: "))
+    if numero % 2 == 0:
+        print(f"El número {numero} es PAR")
+    else:
+        print(f"El número {numero} es IMPAR")
+
+# Llamamos a la función
+verificar_par_impar()
