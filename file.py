5a6f049b3b25c68fec5eddea5106ae1bdf5ad178
Hackejia <hackejia@gmail.com>
2025-09-27 16:57:12 -0500

    1ER Commit: Suma

--- file.py
+++ file.py
@@ -1,2 +1,17 @@
 def repeat_word(word: str, n: int) -> str:
     return " ".join([word] * n)
+
+
+try:
+    # Pedir los números al usuario
+    a = int(input("Ingrese el primer número: "))
+    b = int(input("Ingrese el segundo número: "))
+
+    # Sumar los números
+    c = a + b
+
+    # Mostrar el resultado
+    print(f"La suma de {a} y {b} es igual a: {c}")
+
+except ValueError:
+    print("Error: Debes ingresar solo números enteros.")
