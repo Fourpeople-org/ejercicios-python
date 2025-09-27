f73ca2d215622e92c955d07357edefe90b2e6473
Nicolas <vargascardona12@gmail.com>
2025-09-27 16:37:35 -0500

    fix(Jessika.py): remove file

--- Jessika.py
+++
@@ -1,9 +0,0 @@
-def verificar_par_impar():
-    numero = int(input("Ingresa un número: "))
-    if numero % 2 == 0:
-        print(f"El número {numero} es PAR")
-    else:
-        print(f"El número {numero} es IMPAR")
-
-# Llamamos a la función
-verificar_par_impar()
