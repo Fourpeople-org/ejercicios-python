a492606369f7403766d5e85d68fc45c56e3f4b60
Hackejia <hackejia@gmail.com>
2025-09-27 17:01:30 -0500

    2DO Commit: Suma

--- file.py
+++ file.py
@@ -5,6 +5,16 @@ def is_even(number: int) -> bool:
     """Verifica si un número es par."""
     return number % 2 == 0
 
-def loop():
-   for i in range(1,10):
-     print(i)
+try:
+    # Pedir los números al usuario
+    a = int(input("Ingrese el primer número: "))
+    b = int(input("Ingrese el segundo número: "))
+
+    # Sumar los números
+    c = a + b
+
+    # Mostrar el resultado
+    print(f"La suma de {a} y {b} es igual a: {c}")
+
+except ValueError:
+    print("Error: Debes ingresar solo números enteros.")
